f7bc2ba2243aa23b58b5395194ed7c9ec312f8b3
chromebook <aaugustinwwezel@gmail.com>
2023-12-14 11:14:07 +0100

    Jour 14

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ def create_files_day(number):
     f = open(f"{number}_test.txt", "w")
     f.close()
     f = open(f"{number}_solution.py", "w")
-    f.write("def step_1(filename):\n\tf = open(filename)")
+    f.write("def step_1(filename):\n    f = open(filename)")
     f.write("\n")
     f.write(f"print(step_1('{number}_test.txt'))")
     f.close()
 
 
-create_files_day(13)
+create_files_day(14)
